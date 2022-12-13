17fef76f99d7cc0d43ed4a4b2f2a320989c8b672
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-12-13 16:43:36 +0100

    Update build-hal.sh

--- build-hal.sh
+++ build-hal.sh
@@ -5,13 +5,4 @@ cd $ANDROID_ROOT
 source build/envsetup.sh
 export USE_CCACHE=1
 lunch aosp_f5321-eng
-mka mkbootimg
-mka hybris-boot
-mka halium-boot
-mka recoveryimage
-mka systemimage 
-
-echo "md5sum halium-boot.img and system.img"
-md5sum $ANDROID_ROOT/out/target/product/land/halium-boot.img
-md5sum $ANDROID_ROOT/out/target/product/land/recovery.img
-md5sum $ANDROID_ROOT/out/target/product/land/system.img
+make -i bootimg
